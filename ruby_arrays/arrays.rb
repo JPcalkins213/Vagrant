ff2b8cf832dfa66637f23a5243350b78e4046f63
Jessica Calkins <jessicacalkins795213@gmail.com>
2021-08-03 12:40:53 -0500

    array methods

---
+++ ruby_arrays/arrays.rb
@@ -0,0 +1,23 @@
+a = [1,2,3,4,5,6,7,8,9]
+b = ['Bek', 'Dono', 'Jake', 'Bre', 'Tristan', 'James']
+
+#.at and .fetch
+puts a.fetch(8)
+puts b.fetch(4)
+
+#.delete
+# a.delete(4)
+# b.delete('Jake')
+# puts a
+# puts b
+
+# #.reverse
+# puts a.reverse().to_s
+# puts b.reverse().to_s
+
+# .slice
+# puts a.slice!(4)
+# puts a.to_s
+
+#.shuffle
+puts a.shuffle().to_s
