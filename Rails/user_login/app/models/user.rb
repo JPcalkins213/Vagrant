080d724172211e636faa319e2decbb39c636bc83
Jessica Calkins <jessicacalkins795213@gmail.com>
2021-08-09 14:18:15 -0500

    applying validations

--- Rails/user_login/app/models/user.rb
+++ Rails/user_login/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
     validates :fname, :lname, :email, :age, presence: true
+    validates :age, numericality:{only_integer: true}, inclusion:10..150
+    validates :fname, :lname, length:{minimum: 2}
 end
